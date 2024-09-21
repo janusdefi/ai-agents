14a0c9cb5a2c30bb394cd862cbcbe7144f240792
zane-harvey <zanewilliamharvey@gmail.com>
2024-09-21 17:21:33 -0400

    Create Oracle.sol

---
+++ Oracle.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Oracle is Ownable {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor(address _priceFeed) {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+    }
+
+    function getPrice() external view returns (int) {
+        (, int price, , ,) = priceFeed.latestRoundData();
+        return price;
+    }
+}
